7535d98dc03c47f290070390ed070568b1faa25b
NicklasSoderlund <nicklassoderlund96@gmail.com>
2023-08-18 16:30:30 +0200

    context fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import SocketProvider from './socketContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  //<React.StrictMode>
+  <SocketProvider>
     <App />
-  //</React.StrictMode>,
+    </SocketProvider>
 )
--- client/src/socketContext.tsx
+++ client/src/socketContext.tsx
@@ -1,24 +1,35 @@
-import { PropsWithChildren, createContext, useState } from "react";
+import { PropsWithChildren, createContext, useState, useContext } from "react";
 
 interface ISocketContext {
- username : string
+ username : string,
+ room : string
 }
 
+// Default values for Context
 const defaultValues = {
- username : ""
+ username : "",
+ room : ""
 
 }
 
+// Context to access socketinfo
 const SocketContext = createContext<ISocketContext>(defaultValues);
 
+// Function for using socket
+export const useSocket = () => useContext(SocketContext);
+
+// Provide Context to children components to give them access
 const SocketProvider = ({children}:PropsWithChildren) => {
 
     const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
 
     return (
-        <SocketContext.Provider value={ {username} }>
+        <SocketContext.Provider value={ {username, room} }>
            {children}
         </SocketContext.Provider>
     )
 
 }
+
+export default SocketProvider
