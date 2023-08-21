6e04dd9bde94956d0005c8d24687fb930aaaf9d1
mcsahlin <mcsahlin@outlook.com>
2023-08-21 04:15:30 +0200

    added sockets to server

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import { users, rooms } from './storage/data';
 
 const app = express();
 app.use(cors());
@@ -17,8 +18,38 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
 	console.log('New user connected', socket.id);
 
+	// User connected
 	socket.on('user-connected', (username) => {
+		users.push(username); // add user to users array
+
 		socket.broadcast.emit('new-user-connected', username);
+	});
+
+	// User disconnected
+	socket.on('user-disconnected', (username) => {
+		socket.broadcast.emit('user-disconnected', username);
+	});
+
+	// Create room
+	socket.on('create-room', (roomName) => {
+		rooms.push(roomName); // add room to rooms array
+		socket.broadcast.emit('new-room-created', roomName);
+	});
+
+	// Delete room
+	socket.on('delete-room', (roomName) => {
+		rooms.splice(rooms.indexOf(roomName), 1);
+		socket.broadcast.emit('room-deleted', roomName);
+	});
+
+	// Join room
+	socket.on('join-room', (roomName) => {
+		socket.join(roomName);
+	});
+
+	// Leave room
+	socket.on('leave-room', (roomName) => {
+		socket.leave(roomName);
 	});
 });
 
--- server/storage/data.ts
+++ server/storage/data.ts
@@ -1,13 +1,12 @@
-let users: string[] = [];
-let rooms: string[] = [];
-// let messages: Message[] = [];
-// let roomUsers: RoomUser[] = [];
-// let roomMessages: RoomMessage[] = [];
+export let users: string[] = [];
+export let rooms: string[] = [];
 
-module.exports = { //! Installerat npm i @types/node --save-dev
+module.exports = { //! Installerat npm i @types/node --save-dev 
 	users,
 	rooms,
-	// messages,
-	// roomUsers,
-	// roomMessages,
 };
+// How to use this module in server.ts:
+// import { users, rooms } from './storage/data';
+//
+// console.log(users);
+// console.log(rooms);
