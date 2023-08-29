28d39ab5b5902d33764fc35df28a4ca0e7970a6f
mcsahlin <mcsahlin@outlook.com>
2023-08-29 17:57:00 +0200

    added classnames and wrapper to home.tsx

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -7,43 +7,48 @@ function Home() {
 	const { username, setUsername, enterLobby } = useSocket();
 	const [errorMsg, setErrorMsg] = useState(''); // error message to display if username is invalid
 
-	const [usernameHolder, setUsernameHolder] = useState("")
-	
+	const [usernameHolder, setUsernameHolder] = useState('');
+
 	const navigate = useNavigate();
 
 	return (
-		<>
-			<div>Hello 4Chatters</div>
+		<div className='wrapper'>
+			<div className='login'>
+				<h1 className='login__heading'>Hello 4Chatters</h1>
 
-			<div>
-				<input
-					type='text'
-					onChange={(e) => {
-						setUsernameHolder(e.target.value);
-					}}
-				/>
-				<button
-					onClick={() => {
-						// validate username
-						if (validateUsername(usernameHolder)) {
-							// if valid: set username and enter lobby
-							enterLobby(usernameHolder);
-							navigate('/lobby');
-						} else {
-							// if invalid: display error message
-							setErrorMsg(
-								'Username must be 3-12 characters long and only contain letters and numbers'
-							);
-						}
-					}}
-				>
-					Enter Lobby
-				</button>
+				<div>
+					<input
+						className='login__input'
+						type='text'
+						placeholder='Username (3-12 chars)'
+						onChange={(e) => {
+							setUsernameHolder(e.target.value);
+						}}
+					/>
+					<button
+						className='login__btn'
+						onClick={() => {
+							// validate username
+							if (validateUsername(usernameHolder)) {
+								// if valid: set username and enter lobby
+								enterLobby(usernameHolder);
+								navigate('/lobby');
+							} else {
+								// if invalid: display error message
+								setErrorMsg(
+									'Username must be 3-12 characters long and only contain letters and numbers'
+								);
+							}
+						}}
+					>
+						Enter Lobby
+					</button>
+				</div>
+				<div>
+					<span className='login__error'>{errorMsg}</span>
+				</div>
 			</div>
-			<div>
-				<span className='usernameError'>{errorMsg}</span>
-			</div>
-		</>
+		</div>
 	);
 }
 export default Home;
